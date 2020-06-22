cacdb651bc043265493b8ccf24faea79f5d7bc26
Daniel Inhee Lee <lee.danny112358@gmail.com>
2020-06-22 14:21:18 -0700

    finished

--- TikTokScraper.py
+++ TikTokScraper.py
@@ -1,32 +1,49 @@
-# from requests_html import HTMLSession
-# from bs4 import BeautifulSoup
-#
-# session = HTMLSession()
-#
-# r = session.get('https://www.tiktok.com/tag/bestfandom')
-# r.html.render()
-#
-# soup = BeautifulSoup(r.html.text, 'lxml')
-# print(soup.prettify())
-
-
-# soup = BeautifulSoup(webpage, 'html.parser')
-#
-# print(soup.prettify())
-
-from selenium import webdriver
+import csv
+from bs4 import BeautifulSoup
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-opts = Options()
-opts.add_argument("user-agent=Naverbot")
 
-class TikTokScraper:
-    def __init__(self):
-        self.driver = webdriver.Chrome(chrome_options=opts)
-        self.driver.get('https://www.tiktok.com/tag/bestfandom')
-        sleep(4)
-        videos = self.driver.find_element_by_xpath('//a[@href="<a href="https://www.tiktok.com/@rachmartino/video/6813508636950678790" class="jsx-1792501825 video-feed-item-wrapper"><div class="jsx-1464109409 image-card" style="border-radius: 4px; background-image: url(&quot;https://p16-va-default.akamaized.net/obj/tos-maliva-p-0068/49a0e50f403c410db3f33ead817b9557_1586393606&quot;);"><div class="jsx-3355072868 video-card default"><div class="jsx-3355072868 video-card-mask"></div></div></div></a>"]').click()
-        # individual class markers are jsx-1410658769 video-feed-item
-TikTokScraper()
+opts = Options()
+opts.add_argument("user-agent=Applebot")
+
+driver = webdriver.Chrome(chrome_options=opts)
+driver.get('https://www.tiktok.com/tag/albumlookalike')
+sleep(4)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+sleep(3)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+sleep(3)
+videos = driver.find_element_by_xpath("//script[@type='application/json']")
+page = driver.page_source
+soup = BeautifulSoup(page, 'lxml')
+links = []
+
+for article in soup.find_all('a', class_='jsx-1229794949 video-feed-item-wrapper'):
+    info = []
+    link = article['href']
+    user_link = link[:link.rfind('/v')]
+    info.append(user_link)
+    driver.get(user_link)
+    sleep(4)
+    user_page = driver.page_source
+    user_soup = BeautifulSoup(user_page, 'lxml')
+    description = user_soup.find('meta', attrs={'name':'description'})
+    description = description['content']
+    try:
+        bio = description.split('Fans.')[1]
+        followers = description.split('Likes.')[1].split('F')[0]
+        info.append(followers)
+    except IndexError:
+         bio = 'please find bio manually'
+         info.append(bio)
+    links.append(info)
+    sleep(4)
+
+with open('tiktoknames.csv', 'w') as f:
+    writer = csv.writer(f)
+    for i in range(len(links)):
+        writer.writerow(links[i])
+
+
 
